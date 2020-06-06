2eb6cf954f122c49dff4283a077d1314fb77f425
Les Stuck <les@lesstuck.com>
2020-06-05 20:03:01 -0600

    added node-wav-player for OS sound playback

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const beat = require('./noneuclidean/index.js')
-const howl = require('howler')
+const beat = require('./noneuclidean/index.js');
+// const {Howl, Howler} = require('howler')
+const player = require('node-wav-player');
+
+
+player.play({
+    path: './snd/808_Closed_HH.m4a',
+}).then(() => {
+    console.log('The wav file started to be played successfully.');
+}).catch((error) => {
+    console.error(error);
+});
+
+// var sound = new Howl({
+//     src: ['./snd/808_Closed_HH.m4a']
+// });
+// sound.play();
 
 beat.beat();
