2a2e5f3864a245f7ecaa9de21379520bbc00a57f
Les Stuck <les@lesstuck.com>
2020-06-06 00:27:25 -0600

    mutual require to get function calls back and forth

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const beat = require('./noneuclidean/index.js');
 // const {Howl, Howler} = require('howler')
 const player = require('node-wav-player');
+
+
 var events = require('events');
 const eventEmitter = events.EventEmitter();
 
-player.play({
-    path: './snd/808_Closed_HH.m4a',
-}).then(() => {
-    console.log('The wav file started to be played successfully.');
-}).catch((error) => {
-    console.error(error);
-});
+
+
+const hit = () => {
+    player.play({
+        path: './snd/808_Closed_HH.m4a',
+    }).then(() => {
+        console.log('The wav file started to be played successfully.');
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+
 
 // var sound = new Howl({
 //     src: ['./snd/808_Closed_HH.m4a']
 // });
 // sound.play();
-
+exports.hit = hit;
 beat.beat();
