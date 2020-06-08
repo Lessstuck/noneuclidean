1ad62624f531d1216e323f514da53a081a1e6ff1
Les Stuck <les@lesstuck.com>
2020-06-08 11:24:51 -0600

    implement Track class

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const play = require('./noneuclidean/index.js');
+const Track = require('./noneuclidean/index.js');
 const player = require('node-wav-player');
 
+// const track1 = new Track();
+
+// generate pulse and call noneucledean hit method
 const beat = () => {
     setInterval(() => {
-        let playit = play.play();
-        if (playit == 1) {
+        // let playit = track1.play();
+        // if (playit == 1) {
             hit();
-        };
+        // };
     }, 500);
 }
 
+// play sound
 const hit = () => {
     player.play({
         path: './snd/808_Closed_HH.m4a',
     }).then(() => {
-        console.log('The wav file started to be played successfully.');
+        console.log('play start.');
     }).catch((error) => {
         console.error(error);
     });
 }
 
-
+// turn on pulse-generating beat
 beat();
--- noneuclidean/index.js
+++ noneuclidean/index.js
@@ -1,10 +1,38 @@
-const play = () => {
-    if (.5 < 1) {
-        return 1;
+let count = Math.random(5);
+
+class Track {
+    constructor() {
+        this.name = "name";
+        // this.beatProb = [.33, .33, .33];
+        // this.beatCount = 0;
+        // this.maxBeats = 3;
     }
-    else {
-        return 0;
+    play = () => {
+        if (beatCount == 0) {
+            // play sound
+            beatCount++;
+            return 1;
+        }
+        // elseif (beatCount == maxBeats) {
+        //     // find new maxBeats
+        //     coinToss = Math.random();
+        //     beatProbAccum = 0.;
+        //     for (i = 0; i < this.beatProb.length(); i++)    {
+        //         beatProbAccum = beatProbAccum + this.beatProb[i]
+        //         if (coinToss < beatProbAccum) {
+        //             maxBeats = i + 1 // lengths 1, 2, 3 – convert to dict?
+        //             this.beatCount = 0;
+
+        //             return 0; 
+        //         }
+        //     }
+        // }
+        // else {
+        //     beatCount++; 
+        // }
     }
 }
 
-exports.play = play;
+const track1 = new Track("hh");
+
+exports.Track = Track;
