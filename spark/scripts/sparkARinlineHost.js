be44548d455f378e846af376e350065a497a48f0
Les Stuck <les@lesstuck.com>
2020-06-21 09:56:24 -0600

    deleted physics code from main

--- spark/scripts/sparkARinlineHost.js
+++ spark/scripts/sparkARinlineHost.js
@@ -1,13 +1,10 @@
- const Scene = require('Scene');
+const Scene = require('Scene');
 const Time = require('Time')
-// const CANNON = require('cannon');
 const Diagnostics = require('Diagnostics');
 const Patches = require('Patches');
 const AudioObject = require("sparkar-audio-object");
 const TouchGestures = require('TouchGestures');
 
-// const Noneuclidean = require('noneuclidean');
-// var noneuclidean = new Noneuclidean;
 // Copied from npm module "noneuclidean" without the exports  :(
 let count = Math.random(5);
 function Track(beatProb) {
@@ -48,25 +45,12 @@ const instParams =
     ['player_controller_2', 'player_speaker_2'],
     ['player_controller_3', 'player_speaker_3'],
     ['player_controller_4', 'player_speaker_4']];
-    
-
-// create instruments
-// class Instrument {
-//     constructor(path) {
-//         this.path = path;
-//     }
-//     play = (path) => {
-//         hit(path);
-//     }
-// }
 
 // create arrays of Instrument & Track objects
 const trackCount = instParams.length;
 var instruments = [];
 var tracks = [];
 for (i = 0; i < trackCount; i++) {
-    // let newInstrument = new Instrument(instParams[i]);
-    // instruments.push(newInstrument);
     let newTrack = new Track([.33, .33, .33]);
     newTrack.play();
     tracks.push(newTrack);
@@ -83,86 +67,20 @@ for (i = 0; i < trackCount; i++) {
     players[i].volume = 1.;
 }
 
-// const fallTime = 1000;
-// Reference SphereObject from Scene
 Promise.all([
 Scene.root.findFirst('SphereObject')
 ]).then(function (objects) {
-    // const sphere = objects[0];
-
-    // Create cannon world and setting gravity
-    // const world = new CANNON.World();
-    // world.gravity.set(0, -9.82, 0);
-    
-    // Create sphere body and setting its shape and properties
-    // const radius = 1;
-    // const sphereProps = {
-    //     mass: 5,
-    //     position: new CANNON.Vec3(0, 10, 0),
-    //     radius: radius,
-    //     shape: new CANNON.Sphere(radius),
-    // }
-    // const sphereBody = new CANNON.Body(sphereProps);
-    // world.addBody(sphereBody);
-
-    // Create ground body and settings its shape and properties
-    // const groundProps = {
-    //     mass: 0,
-    //     position: new CANNON.Vec3(0, -sphereProps.radius, 0),
-    //     shape: new CANNON.Plane(),
-    // }
-    // const groundBody = new CANNON.Body(groundProps);
-    // // Rotate the ground so it is flat (facing upwards)
-    // const angle = -Math.PI / 2;
-    // const xAxis = new CANNON.Vec3(1, 0, 0);
-    // groundBody.quaternion.setFromAxisAngle(xAxis, angle);
-    // world.addBody(groundBody);
-
-
-    // Configure time step for Cannon
-    // const fixedTimeStep = 1.0 / 60.0;
-    // const maxSubSteps = 3;
-    // const timeInterval = 30;
-    // let lastTime;
-    // let freeFalling = 1;
-    // let lastFallTime;
-    // let fallTime = 3000;
-    
-
-
-    // collision listener
-    // sphereBody.addEventListener("collide", function (event) {
-    //     player.play();
-    // });
-
-
-
-    // retrigger drop
     TouchGestures.onTap().subscribe(function (event) {
         for (j = 0; j < trackCount; j++) {
             if (tracks[j].play() == 1) {
                 players[j].play();
             };
         }
-
-        // sphereBody.position.x = 0;
-        // sphereBody.position.y = 10;
-        // sphereBody.position.z = 0;
     });
 
     // update
     var idInterval = Time.setInterval(function (time) {
-        // if (lastTime !== undefined) {
-        //     let dt = (time - lastTime) / 1000;
-        //     world.step(fixedTimeStep, dt, maxSubSteps)
-        //     sphere.transform.x = sphereBody.position.x;
-        //     sphere.transform.y = sphereBody.position.y;
-        //     sphere.transform.z = sphereBody.position.z;
-        // };
-        // lastTime = time
+
     }, timeInterval);
 
 });        
-
-
-
