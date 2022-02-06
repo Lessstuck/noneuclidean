2d370ed598a5320a4f736878062af7d796a8dcc3
Lessstuck <38271153+Lessstuck@users.noreply.github.com>
2022-02-06 14:42:15 -0700

    First version of multi-format build

---
+++ dev/rollup.config.js
@@ -0,0 +1,13 @@
+let formats = ['iife', 'es', 'cjs']
+
+export default formats.map(function(format) {
+    return{
+        input: 'noneuclidean.js',
+        output: {
+            file: `../build/noneuclidean.${format}.js`,
+            format: format,
+            name: 'noneuclidean'
+        }
+    }
+})
+    
